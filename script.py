41e6a1e5335a2edb7a457e82c39feefe3da015e5
Neel Dutta <neel.dutta0729@gmail.com>
2026-05-13 23:47:24 +0530

    final draft

--- script.py
+++ script.py
@@ -1,19 +1,176 @@
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
 
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install())
+)
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+print("\nOpening live match page")
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+driver.get("https://www.cricketworld.com/cricket/live")
 
-<p class="story">...</p>
-"""
-soup = BeautifulSoup(html_doc, 'html.parser')
+time.sleep(5)
 
-print(soup.prettify())
+competitions = driver.find_elements(
+    By.CLASS_NAME,
+    "competition"
+)
+
+print("Competitions Found:", len(competitions))
+
+ipl_link = ""
+
+for comp in competitions:
+
+    try:
+
+        heading = comp.find_element(
+            By.TAG_NAME,
+            "h2"
+        )
+
+        heading_text = heading.text.strip()
+
+        print("\nTournament:")
+        print(heading_text)
+
+        if "Indian Premier League" in heading_text:
+
+            print("\nIPL FOUND")
+
+            match = comp.find_element(
+                By.CLASS_NAME,
+                "match-row"
+            )
+
+            ipl_link = match.get_attribute("href")
+
+            print("IPL LINK:")
+            print(ipl_link)
+
+            break
+
+    except Exception as e:
+
+        print("Competition Error:", e)
+
+if ipl_link == "":
+
+    print("\nNo IPL live match found")
+
+    driver.quit()
+
+    exit()
+
+old_events = ""
+
+while True:
+
+    try:
+
+        print("\nRefreshing IPL match page")
+
+        driver.get(ipl_link)
+
+        time.sleep(5)
+
+        recent_div = driver.find_element(
+            By.CLASS_NAME,
+            "live-info4"
+        )
+
+        spans = recent_div.find_elements(
+            By.TAG_NAME,
+            "span"
+        )
+
+        print("\nEvents Found:", len(spans))
+
+        events = []
+
+        for span in spans:
+
+            text = span.text.strip()
+
+            span_class = span.get_attribute("class")
+
+            event = {
+                "text": text,
+                "class": span_class
+            }
+
+            events.append(event)
+
+        print("\nALL EVENTS:")
+
+        for e in events:
+
+            print(e)
+
+        current_events = str(events)
+
+        if current_events != old_events:
+
+            print("\nNEW UPDATE DETECTED")
+
+            if len(events) > 0:
+
+                latest_event = events[0]
+
+                latest_text = latest_event["text"]
+
+                latest_class = latest_event["class"]
+
+                print("\nLATEST EVENT:")
+                print("Text :", latest_text)
+                print("Class:", latest_class)
+
+                if "run4" in latest_class:
+
+                    print("\nFOUR!")
+
+                    os.system("""
+                    osascript -e 'display notification "Boundary scored" with title "🏏 FOUR"'
+                    """)
+
+                elif "run6" in latest_class:
+
+                    print("\nSIX!")
+
+                    os.system("""
+                    osascript -e 'display notification "Maximum scored" with title "🏏 SIX"'
+                    """)
+
+                elif "wicket" in latest_class.lower():
+
+                    print("\nWICKET!")
+
+                    os.system("""
+                    osascript -e 'display notification "Batsman Out" with title "🏏 WICKET"'
+                    """)
+
+                else:
+
+                    print("\nNormal ball event")
+
+            old_events = current_events
+
+        else:
+
+            print("\nNo new updates")
+
+        print("\nWaiting 15 seconds...\n")
+
+        time.sleep(15)
+
+    except Exception as e:
+
+        print("\nERROR:", e)
+
+        print("\nRetrying in 15 seconds...\n")
+
+        time.sleep(15)
